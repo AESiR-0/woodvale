8141ef22423e21c8e61c636386ee629a2177308e
Prat <90002355+AESiR-0@users.noreply.github.com>
2025-10-13 15:49:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Hero from "@/sections/Hero";
+import Image from "next/Image"
 
 export default function Home() {
   return (
